f8cda038b5c8fbd61c7c25878005a9e2e045da09
progspac-vnn <vineshnnadar@gmail.com>
2023-08-25 21:46:48 +0530

    Final Check

--- backend/redditAPI/views.py
+++ backend/redditAPI/views.py
@@ -2,40 +2,44 @@ from datetime import datetime
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.cache import cache
 from .serializers import RedditSerializer
 import praw
 from django.conf import settings
 import re
 
-# Loader Testing
-
 class RedditAPIView(APIView):
     def get(self, request, sub):
-
         try:
             subreddit_name = sub
             print("Subreddit Name:", subreddit_name)
+
+            # Check if data is cached
+            cached_data = cache.get(f'reddit_{sub}')
+            if cached_data:
+                return Response(cached_data, status=status.HTTP_200_OK)
+
             # API credentials
             reddit = praw.Reddit(
-                client_id = settings.REDDIT_CLIENT_ID,
-                client_secret = settings.REDDIT_CLIENT_SECRET,
+                client_id=settings.REDDIT_CLIENT_ID,
+                client_secret=settings.REDDIT_CLIENT_SECRET,
                 username='progspac-vnn',
                 password='Vinesh@2703',
-                user_agent = 'test-api', # app name
+                user_agent='test-api',
             )
 
-            # Fetch 10 data from reddit
+            # Fetch 10 data from Reddit
             subreddit = reddit.subreddit(subreddit_name) if subreddit_name else reddit.subreddit('movies')
             hot_posts = subreddit.hot(limit=10)
 
-            # Process and serialize the data 
+            # Process and serialize the data
             serialized_posts = []
 
             for post in hot_posts:
                 post_data = {
                     'title': post.title,
                     'author': post.author.name,
-                    'created_at': datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S'),  # Format the datetime
+                    'created_at': datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S'),
                     'content': post.selftext,
                     'image_url': post.url if post.url.endswith(('.png', '.jpg', '.jpeg', '.gif')) else None,
                     'video_url': None,
@@ -49,6 +53,10 @@ class RedditAPIView(APIView):
 
             serializer = RedditSerializer(data=serialized_posts, many=True)
             serializer.is_valid()
+
+            # Cache the data for 15 minutes
+            cache.set(f'reddit_{sub}', serializer.data, 60 * 15)
+
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'error' : str(e)}, status = status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- my_locustfile.py
+++ my_locustfile.py
@@ -1,9 +1,9 @@
 from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
-    wait_time = between(1, 3)  # Wait between 1 and 3 seconds between tasks
+    wait_time = between(1, 3)
     
     @task
     def get_reddit_posts(self):
-        subreddit = "your-subreddit-name"  # Replace with the subreddit you want to load test
-        self.client.get(f"/reddit/view/{subreddit}/")
+        subreddit = "java"  
+        self.client.get(f"api/reddit/view/{subreddit}")
